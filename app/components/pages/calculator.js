a61d35ca94725bf1ecd095bf7a3244300d93c5ad
Calvin Collins <calvinkcollins@gmail.com>
2017-08-27 09:15:41 -0400

    working random numbers

--- app/components/pages/calculator.js
+++ app/components/pages/calculator.js
@@ -4,18 +4,34 @@ import styles from 'app/styles';
 
 export default class CalculatorPage extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      revealAnswer: false,
+      answer: false,
+      number1: null,
+      number2: null,
+    };
+  }
+
+  getRandomNumberFromDigits(digits) {
+    const tenthPower = Math.pow(10, digits);
+    return Math.floor(Math.random() * tenthPower);
+  }
 
   render() {
     return (
       <View style={[styles.container, styles.column]}>
         <View style={styles.calcContainer}>
-          <Text style={styles.number}>{3488}</Text>
+          <Text style={styles.number}>{this.getRandomNumberFromDigits(this.props.digitCount1)}</Text>
           <Text style={[styles.number, styles.lastNumber]}>
             {this.props.operator}
             {"    "}
-            {348}
+            {this.getRandomNumberFromDigits(this.props.digitCount2)}
           </Text>
-          <Text style={[styles.number]}>{888923984}</Text>
+          {this.state.revealAnswer &&
+            <Text style={[styles.number]}>{this.state.answer}</Text>
+          }
         </View>
       </View>
     );
